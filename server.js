a568ec1c2c14d6cd5b85e1c65d6811d9ac993e53
Russell Montgomery <119378062+Rmontgomery4801@users.noreply.github.com>
2023-04-24 15:09:53 -0700

    added files and made changes

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require("./public"));
+app.use(require('./routes'));
 
-app.use(require("./routes"));
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
-  {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    // useFindAndModify: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+    useUnifiedTopology: true
+});
 
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+    console.log(`App running on port ${PORT}!`);
+    });
